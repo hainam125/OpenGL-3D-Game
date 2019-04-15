a4a735b22569fe0f78a6eac8e07830ba4aa17113
Nam Nguyen <hainam125@gmail.com>
2019-04-15 22:09:51 +0700

    MVP

---
+++ src/entities/Camera.java
@@ -0,0 +1,48 @@
+package entities;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector3f;
+
+public class Camera {
+	private Vector3f position = new Vector3f(0,0,2);
+	private float pitch;//how high or low the camera is aimed
+	private float yaw;//how much left or right camera is aiming
+	private float roll;//how much camera is tilted to one side
+	
+	public Camera() {
+		
+	}
+	
+	public void move() {
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			position.z -= 0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			position.z += 0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			position.x += 0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			position.x -= 0.02f;
+		}
+	}
+
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+	
+	
+}
